64f02391fc828027f3b0b88f32490d83e382ccc0
Thiago B Medeiros <120425620+u002169@users.noreply.github.com>
2025-01-31 22:51:50 -0300

    Update report.js

--- src/report.js
+++ src/report.js
@@ -61,8 +61,9 @@ export const buildReportSummary = async (analysisResult) => {
 
 	await core.summary
 		.addHeading('SonarQube Report')
-		.addTable(tableSummary)
-		.write();
+		.addTable(tableSummary);
+	core.summary.configureAlignmentOptions(['left', 'left', 'center']);
+	core.summary.write();
 };
 
 const buildRowSummary = (row, tableSummary) => {
